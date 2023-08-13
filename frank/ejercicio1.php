fdad7fde7a1f9a2999bddf5342956bcd24937eff
ArimaAshura <frankmateo3@gmail.com>
2023-08-13 15:35:26 -0500

    Ejercicos

---
+++ frank/ejercicio1.php
@@ -0,0 +1,93 @@
+<?php
+// $num1 = 10;
+// $num2 = 20;
+// $num3 = 30;
+// $promedio = $num1 + $num2 + $num3 / 3;
+// echo "el promedio es = " . $promedio;
+
+// function promedio(int $num1, int $num2, int $num3)
+// {
+//     $promedio = $num1 + $num2 + $num3 / 3;
+//     return $promedio;
+// }
+
+// $promedio = promedio(10, 20, 30);
+// $promedio2 = promedio(5, 69, 34);
+// echo "El promedio 1 es : " . $promedio . " y el promedio 2 es: " . $promedio2;
+
+// $promedio = " feliz dia";
+// echo $promedio;
+
+// class Promedio
+// {
+//     public function resultadoPromedio($num1, $num2, $num3)
+//     {
+//         $promedio = $num1 + $num2 + $num3 / 3;
+//         return $this->mensaje() . $promedio;
+//     }
+//     private function mensaje()
+//     {
+//         return "El promedio es: ";
+//     }
+// }
+
+// $promedioClass = new Promedio();
+// $promedio = $promedioClass->resultadoPromedio(54, 23, 56);
+// echo $promedio;
+
+
+// si el promedio es mayor o igual a 2.9 pierde, si es mayor a 3 gana.
+// ejemplo 
+
+// $nota = 2.9;
+// if ($nota>=2.9) {
+//     echo "gano el año";
+
+// }else {
+//     Echo "perdio el año";
+// }
+
+// $nota1 = 3;
+// $nota2 = 2;
+// $nota3 = 5;
+// $nota4 = 4;
+// $nota5 = 1;
+// $promedio = ($nota1 + $nota2 + $nota3 + $nota4 + $nota5) / 5;
+
+// if ($promedio >= 2.9) {
+//     echo "Gano el Año con ". $promedio;
+// } else {
+//     echo "perdio el año con ". $promedio;
+// }
+
+
+// function notaFinal($nota1, $nota2, $nota3, $nota4, $nota5)
+// {
+//     $promedio = ($nota1 + $nota2 + $nota3 + $nota4 + $nota5) / 5;
+//     return $promedio;
+// }
+
+// $notaFinal = notaFinal(3, 4, 2, 5, 1.6);
+// if ($notaFinal >= 2.9) {
+//     echo "Gano el Año con ".$notaFinal;
+// } else {
+//     echo "perdio el año con ".$notaFinal;
+// }
+
+// class NotaFinal
+// {
+//     public function notaFinal($nota1, $nota2, $nota3, $nota4, $nota5)
+//     {
+//         $promedio = ($nota1 + $nota2 + $nota3 + $nota4 + $nota5) / 5;
+//         return $promedio;
+//     }
+// }
+
+// $NotaFinalClass = new NotaFinal();
+// $notaFinal = $NotaFinalClass->notaFinal(2, 3, 4, 1, 4);
+// if ($notaFinal >= 2.9) {
+//     echo "Gano el Año con " . $notaFinal;
+// } else {
+//     echo "perdio el año con " . $notaFinal;
+// }
+
