26e9a7bd79956c5f355764d3231574fb62f10a2f
ArimaAshura <frankmateo3@gmail.com>
2023-08-13 18:39:11 -0500

    ejercicios4

---
+++ frank/ejercicio4.php
@@ -0,0 +1,269 @@
+<?php
+
+// $Dia = 8;
+
+// switch ($Dia) {
+//     case '1':
+//         return "es lunes";
+//         break;
+//     case '2':
+//         return "Es Martes";
+//         break;
+//     case '3':
+//         return "es Miercoles";
+//         break;
+//     case '4':
+//         return "Es jueves";
+//         break;
+//     case '5':
+//         return "es Viernes";
+//         break;
+//     case '6':
+//         return "Es sabado";
+//         break;
+//     case '7':
+//         return "es domingo";
+//         break;
+//     default:
+//         return "No es un dia de la semana";
+//         break;
+// }
+
+
+/// FUNCION
+
+// function dia($Dia)
+// {
+//     switch ($Dia) {
+//         case '1':
+//             return "es lunes";
+//             break;
+//         case '2':
+//             return "Es Martes";
+//             break;
+//         case '3':
+//             return "es Miercoles";
+//             break;
+//         case '4':
+//             return "Es jueves";
+//             break;
+//         case '5':
+//             return "es Viernes";
+//             break;
+//         case '6':
+//             return "Es sabado";
+//             break;
+//         case '7':
+//             return "es domingo";
+//             break;
+//         default:
+//             return "No es un dia de la semana";
+//             break;
+//     }
+// }
+
+// $Dia = 4;
+
+// $Dia = dia($Dia);
+// echo $Dia;
+
+
+//// CLASS
+
+// class dia
+// {
+//     public function dia($Dia)
+//     {
+//         switch ($Dia) {
+//             case '1':
+//                 return "es lunes";
+//                 break;
+//             case '2':
+//                 return "Es Martes";
+//                 break;
+//             case '3':
+//                 return "es Miercoles";
+//                 break;
+//             case '4':
+//                 return "Es jueves";
+//                 break;
+//             case '5':
+//                 return "es Viernes";
+//                 break;
+//             case '6':
+//                 return "Es sabado";
+//                 break;
+//             case '7':
+//                 return "es domingo";
+//                 break;
+//             default:
+//                 return "No es un dia de la semana";
+//                 break;
+//         }
+//     }
+// }
+
+// $Dia = 7;
+
+// $diaclass = new dia;
+// $Dia = $diaclass->dia($Dia);
+// echo $Dia;
+
+
+
+//// EJERCICIO MESES
+
+
+$Mes = 8;
+
+switch ($Mes) {
+    case '1':
+        return "es enero";
+        break;
+    case '2':
+        return "Es febrero";
+        break;
+    case '3':
+        return "es Marzo";
+        break;
+    case '4':
+        return "Es Abril";
+        break;
+    case '5':
+        return "es Mayo";
+        break;
+    case '6':
+        return "Es Junio";
+        break;
+    case '7':
+        return "es Julio";
+        break;
+    case '8':
+        return "es Agosto";
+        break;
+    case '9':
+        return "Es Septiembre";
+        break;
+    case '10':
+        return "es octubre";
+        break;
+    case '11':
+        return "Es Noviembre";
+        break;
+    case '12':
+        return "es diciembre";
+        break;
+    default:
+        return "No es un mes valido";
+        break;
+}
+
+
+// // FUNCION
+
+// function mes($Mes)
+// {
+//     switch ($Mes) {
+//         case '1':
+//             return "es enero";
+//             break;
+//         case '2':
+//             return "Es febrero";
+//             break;
+//         case '3':
+//             return "es Marzo";
+//             break;
+//         case '4':
+//             return "Es Abril";
+//             break;
+//         case '5':
+//             return "es Mayo";
+//             break;
+//         case '6':
+//             return "Es Junio";
+//             break;
+//         case '7':
+//             return "es Julio";
+//             break;
+//         case '8':
+//             return "es Agosto";
+//             break;
+//         case '9':
+//             return "Es Septiembre";
+//             break;
+//         case '10':
+//             return "es octubre";
+//             break;
+//         case '11':
+//             return "Es Noviembre";
+//             break;
+//         case '12':
+//             return "es diciembre";
+//             break;
+//         default:
+//             return "No es un mes valido";
+//             break;
+//     }
+// }
+
+// $Mes = 4;
+
+// $Mes = mes($Mes);
+// echo $Mes;
+
+
+// CLASS
+
+// class mes
+// {
+//     public function mes($Mes)
+//     {
+//         switch ($Mes) {
+//             case '1':
+//                 return "es enero";
+//                 break;
+//             case '2':
+//                 return "Es febrero";
+//                 break;
+//             case '3':
+//                 return "es Marzo";
+//                 break;
+//             case '4':
+//                 return "Es Abril";
+//                 break;
+//             case '5':
+//                 return "es Mayo";
+//                 break;
+//             case '6':
+//                 return "Es Junio";
+//                 break;
+//             case '7':
+//                 return "es Julio";
+//                 break;
+//             case '8':
+//                 return "es Agosto";
+//                 break;
+//             case '9':
+//                 return "Es Septiembre";
+//                 break;
+//             case '10':
+//                 return "es octubre";
+//                 break;
+//             case '11':
+//                 return "Es Noviembre";
+//                 break;
+//             case '12':
+//                 return "es diciembre";
+//                 break;
+//             default:
+//                 return "No es un mes valido";
+//                 break;
+//         }
+//     }
+// }
+
+// $Mes = 7;
+
+// $mesclass = new mes;
+// $Mes = $mesclass->mes($Mes);
+// echo $Mes;
